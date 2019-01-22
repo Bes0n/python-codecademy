e77a2fbadab399276298f599fe6c2eaa6232a3f4
Ilgar <innaghiyev@gmail.com>
2019-01-22 11:39:59 +0100

    Update tip_calculator.py

--- projects/tip_calculator.py
+++ projects/tip_calculator.py
@@ -3,3 +3,12 @@ Now that you have completed the lesson on Python syntax, it's time to practice y
 
 If you get stuck during this project, check out the project walkthrough video which can be found at the bottom of the page after the final step of the project.
 """
+
+meal = 44.50
+tax = .0675 #which equals to 6.75%
+tip = .15 #which equals to 15% 
+
+meal += meal * tax 
+total = meal + meal * tip
+
+print '%02d.02' % (total)
