4506ff16a6c631fad20955d038fde4812c343a7f
Ilgar <innaghiyev@gmail.com>
2019-01-22 11:07:32 +0100

    Create tip_calculator.py

---
+++ projects/tip_calculator.py
@@ -0,0 +1,5 @@
+"""
+Now that you have completed the lesson on Python syntax, it's time to practice your newfound skills. In this project, you will create a simple calculator that determines the price of a meal after tax and tip.
+
+If you get stuck during this project, check out the project walkthrough video which can be found at the bottom of the page after the final step of the project.
+"""
