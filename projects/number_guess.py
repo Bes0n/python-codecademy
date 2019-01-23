343c0d381e0f72ccc9b095b2c92cd60067c64cdf
Timur Aitov <timonbl4@gmail.com>
2019-01-23 13:12:40 +0300

    support python3

--- projects/number_guess.py
+++ projects/number_guess.py
@@ -1,15 +1,12 @@
 ### Number Guess
 
 """ Wanna play a game? In this project, we'll build a program that rolls a pair of dice and asks the user to guess the sum. If the user's guess is equal to the total value of the dice roll, the user wins! Otherwise, the computer wins.
-
 The program should do the following:
-
     Roll a pair of dice.
     Add the values of the roll.
     Ask the user to guess a number.
     Compare the user's guess to the total value.
     Determine the winner (user or computer).
-
 Let's begin! """
 
 #To make sure that the rolls are random, we will need some Python code that isn't built-in  we need to import the randint function, from the random module.
@@ -20,7 +17,7 @@ from time import sleep
 
 #By default, using raw_input alone will store the user's input as a string. Since the user is guessing a whole number, we will need an integer, not a string. 
 def get_user_guess():
-  guess = int(raw_input("Guess a number: "))
+  guess = int(input("Guess a number: "))
   return guess 
 #The roll_dice function will be used to simulate the rolling of a pair of dice. 
 def roll_dice(number_of_sides):
@@ -29,24 +26,24 @@ def roll_dice(number_of_sides):
   second_roll = randint(1, int(number_of_sides))
   #On the next line, create a variable called max_val and set it equal to number_of_sides times 2 (since there are two dice).
   max_val = number_of_sides * 2
-  print "Max possible value: %d" % (max_val)
+  print("Max possible value: %s" % (max_val))
   #On the next line, call the get_user_guess() function. Remember that the function will return the user's guess after prompting the user.
 	#Store the returned value into a variable called guess.
   guess = get_user_guess()
   if guess > max_val:
-    print "Your guess is greater that the maximum value."
+    print("Your guess is greater that the maximum value.")
   else:
-    print "Rolling..."
+    print("Rolling...")
     sleep(2)
     #Print roll numbers 
-    print "First roll gave number: %d" % (first_roll)
+    print("First roll gave number: %d" % (first_roll))
     sleep(1)
-    print "Second roll gave number: %d" % (second_roll)
+    print("Second roll gave number: %d" % (second_roll))
     sleep(1)
     #get total of first and second roll 
     total_roll = first_roll + second_roll
-    print "Total roll is %d" % (total_roll)
-    print "Result..."
+    print("Total roll is %d" % (total_roll))
+    print("Result...")
     sleep(1)
     #if you guessed number right - you won, else you lost
     if guess == total_roll:
