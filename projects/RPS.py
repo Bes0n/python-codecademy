63d42f805c06679df20c77d7432939a5b0f4d39d
Ilgar <innaghiyev@gmail.com>
2019-01-24 12:12:27 +0100

    Create RPS.py

---
+++ projects/RPS.py
@@ -0,0 +1,17 @@
+""" 
+Rock, Paper, Scissors
+
+<div class="theme__22QeW-d-YRjfwg7z9oiZH_"><p>In this project, we'll build <a href="https://en.wikipedia.org/wiki/Rock-paper-scissors" target="_blank" rel="noopener noreferrer">Rock-Paper-Scissors</a>!</p>
+<p>The program should do the following:</p>
+<ol>
+<li>Prompt the user to select either Rock, Paper, or Scissors.</li>
+<li>Instruct the computer to randomly select either Rock, Paper, or Scissors.</li>
+<li>Compare the user's choice and the computer's choice.</li>
+<li>Determine a winner (the user or the computer).</li>
+<li>Inform the user who the winner is.</li>
+</ol>
+<p>Happy coding!</p>
+<p>If you get stuck during this project, check out the <strong>project walkthrough video</strong> which can be found at the bottom of the page after the final step of the project.</p>
+</div>
+
+"""
