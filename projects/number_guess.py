555c1510ede9c266ce9375aea81a821092504c8c
Ilgar <innaghiyev@gmail.com>
2019-01-24 14:34:18 +0100

    Update number_guess.py

--- projects/number_guess.py
+++ projects/number_guess.py
@@ -1,6 +1,7 @@
 ### Number Guess
 
-""" Wanna play a game? In this project, we'll build a program that rolls a pair of dice and asks the user to guess the sum. If the user's guess is equal to the total value of the dice roll, the user wins! Otherwise, the computer wins.
+""" Wanna play a game? In this project, we'll build a program that rolls a pair of dice and asks the user to guess the sum. 
+If the user's guess is equal to the total value of the dice roll, the user wins! Otherwise, the computer wins.
 The program should do the following:
     Roll a pair of dice.
     Add the values of the roll.
