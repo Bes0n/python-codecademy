06ee0506a8bcbad962600d9283f2897a97052ce2
Ilgar <innaghiyev@gmail.com>
2019-01-29 13:35:03 +0100

    Create command_line_calendar.py

---
+++ projects/command_line_calendar.py
@@ -0,0 +1,17 @@
+"""
+Command Line Calendar
+
+So far, you've used Python to build a variety of things, including calculators and games. In this project, we'll build a basic calendar that the user will be able to interact with from the command line. The user should be able to choose to:
+
+    * View the calendar
+    * Add an event to the calendar
+    * Update an existing event
+    * Delete an existing event
+
+The program should behave in the following way:
+
+    1. Print a welcome message to the user
+    2. Prompt the user to view, add, update, or delete an event on the calendar
+    3. Depending on the user's input: view, add, update, or delete an event on the calendar
+    4. The program should never terminate unless the user decides to exit
+"""
